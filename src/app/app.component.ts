a5ced535cd7e400acf8f36ea4e72d056a7cdef8a
shiv <shivakumarsuryan@gmail.com>
2019-10-26 12:30:55 +0530

    Added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template urls accepts single html.
   templateUrl: './app.component.html',
+  // style urls accepts multiple style sheets as string array arguments.
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {

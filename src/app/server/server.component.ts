b5ef68887e2a661db42e32837498d783f793a700
shivakumarsuryan@gmail.com <shivakumarsuryan@gmail.com>
2019-10-26 16:37:29 +0530

    Using property binding to enable/disable button.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId: number = 15;
     serverLocation: string = 'California';
+    // this property is used to enable disable the button 
+    addNewserver: boolean = false;
+
+    constructor() {
+        setTimeout(() => {
+            this.addNewserver = true;
+        }, 5000);
+    }
 
     getServerLocation() {
         return this.serverLocation;

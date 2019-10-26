d9aae6db1cffdb33b7898f23deed00f211bd3798
shivakumarsuryan@gmail.com <shivakumarsuryan@gmail.com>
2019-10-26 16:06:31 +0530

    String Interpolation example.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 // Indicating typescript that this is an angular component
 @Component({
-    selector : 'app-server',
-    templateUrl : './server.component.html'
+    selector: 'app-server',
+    templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverId: number = 15;
+    serverLocation: string = 'California';
 
+    getServerLocation() {
+        return this.serverLocation;
+    }
 }

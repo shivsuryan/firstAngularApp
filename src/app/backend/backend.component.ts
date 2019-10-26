3c1160354ca24b008d4e3a48f605c14629c27e58
shivakumarsuryan@gmail.com <shivakumarsuryan@gmail.com>
2019-10-26 14:59:37 +0530

    Using attribute type of selector.

--- src/app/backend/backend.component.ts
+++ src/app/backend/backend.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-backend',
+  selector: '[app-backend]',
   templateUrl: './backend.component.html',
   styleUrls: ['./backend.component.css']
 })

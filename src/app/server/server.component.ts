a087050e78ed49087325327095dddb1a1ccecb43
shiv <shivakumarsuryan@gmail.com>
2019-10-26 10:42:55 +0530

    Add custom server component and display in main component.

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// Indicating typescript that this is an angular component
+@Component({
+    selector : 'app-server',
+    templateUrl : './server.component.html'
+})
+export class ServerComponent {
+
+}
